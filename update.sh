2329805d68128c58a590ed3581aa189843fb79df
Damian Zaremba <damian@damianzaremba.co.uk>
2011-08-01 16:11:35 +0100

    Adding in update script

---
+++ update.sh
@@ -0,0 +1,17 @@
+cd ~;
+if [ -d ".files/.git" ];
+then
+	cd ~/.files/;
+	git pull;
+	cd ~;
+else
+	git clone git://github.com/DamianZaremba/.files.git;
+fi
+
+if [ ! -d ".files/" ];
+then
+	echo "~/.files/ does not exist";
+	exit 2;
+fi
+
+rsync -avr --exclude='update.sh' --exclude='README' ~/.files/* ~
